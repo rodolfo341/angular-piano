8ced076a401ef1bc7014a3ad2a4b65c379ccf56f
Rodolfo Aravena <rodolfo.aravena.espinosa@gmail.com>
2022-10-10 10:19:23 -0300

    terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'piano-angular';
+    title = 'piano-angular';
+
+    aplicarSonido(num: number): void {
+        const audio = new Audio();
+        audio.src = "../assets/sonidos/note" + num + ".wav";
+        audio.load();
+        audio.play();
+    }
 }
